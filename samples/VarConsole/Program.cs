97d1a156e484d90f04aee0380394d54cfdd9c460
13angs <13angz.um@gmail.com>
2024-10-04 09:55:15 +0000

    Add VarConsole sample

---
+++ samples/VarConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿using DotNetEnv;
+
+Env.Load();
+// See https://aka.ms/new-console-template for more information
+var name = Environment.GetEnvironmentVariable("NAME");
+var hobby = Environment.GetEnvironmentVariable("HOBBY");
+var job = Environment.GetEnvironmentVariable("JOB");
+Console.WriteLine($"Name: {name}\nHobby: {hobby}\nJob: {job}");
